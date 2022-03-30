33f4ec9de7ce0e090afac907fcfff0b775942af5
Prachi Sahani <prachirsahani97@gmail.com>
2022-03-31 00:44:33 +0530

    fix - changes suggested in PR review

--- src/Components/sidenav/Sidenav.jsx
+++ src/Components/sidenav/Sidenav.jsx
@@ -1,34 +1,16 @@
-import { Link } from "react-router-dom";
-import "./sidenav.css"
-import "../../styles.css"
+import "./sidenav.css";
+import "../../styles.css";
+import { SidenavItem } from "./SidenavItem";
+import { sidenavItemList } from "./sidenavItemList";
 export function Sidenav() {
   return (
-    <div className="sidenav py-4 px-2">
+    <div className="sidenav px-2">
       <ul className="list-group-stacked">
-        <Link to="/" className="link"><li className="list-item txt-center">
-          <i className="material-icons-outlined list-icon">home</i>
-          <p className="txt txt-sm">Home</p>
-        </li></Link>
-        <Link to="/explore" className="link"><li className="list-item txt-center">
-          <i className="material-icons-outlined list-icon">explore</i>
-          <p className="txt txt-sm">Explore</p>
-        </li></Link>
-        <Link to="/playlists" className="link"><li className="list-item txt-center">
-          <i className="material-icons-outlined list-icon">library_music</i>
-          <p className="txt txt-sm">Playlists</p>
-        </li></Link>
-        <Link to="/watchLater" className="link"><li className="list-item txt-center">
-          <i className="material-icons-outlined list-icon">watch_later</i>
-          <p className="txt txt-sm">Watch Later</p>
-        </li></Link>
-        <Link to="/likedVideos" className="link"><li className="list-item txt-center">
-          <i className="material-icons-outlined list-icon">thumb_up</i>
-          <p className="txt txt-sm">Liked Videos</p>
-        </li></Link>
-        <Link to="/history" className="link"><li className="list-item txt-center">
-          <i className="material-icons list-icon">history</i>
-          <p className="txt txt-sm">History</p>
-        </li></Link>
+        {sidenavItemList.map((item) => (
+          <li key={item.title} className="list-item txt-center">
+            <SidenavItem item={item} />
+          </li>
+        ))}
       </ul>
     </div>
   );
