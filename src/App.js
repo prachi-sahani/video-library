4c417a1b8da36b473963557b37629c502036656e
Prachi Sahani <64582473+prachi-sahani@users.noreply.github.com>
2022-04-06 20:47:43 +0530

    change components to Components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
-// import {
-// HomePage,
-//   Navbar,
-//   Footer,
-//   PageNotFound,
-//   ExplorePage,
-//   LoginPage,
-//   SignupPage,
-// } from "";
-
-import { HomePage } from "./components/homePage/HomePage";
-import { SignupPage } from "./components/login/SignupPage";
-import { LoginPage } from "./components/login/LoginPage";
-import { Navbar } from "./components/navbar/Navbar";
-import { Footer } from "./components/footer/Footer";
-import { PageNotFound } from "./components/pageNotFound/PageNotFound";
-import { ExplorePage } from "./components/explorePage/ExplorePage";
-
-import { Snackbar } from "./components/snackbar/Snackbar";
+import {
+HomePage,
+  Navbar,
+  Footer,
+  PageNotFound,
+  ExplorePage,
+  LoginPage,
+  SignupPage,
+} from "./Components/index";
+import { Snackbar } from "./Components/snackbar/Snackbar";
 import { useMessageHandling } from "./context/message-handling";
 function App() {
   const{errorMessage} = useMessageHandling()
