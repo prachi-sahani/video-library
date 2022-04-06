86c956da866695119f5be7e0f3f0029fdcce9a0b
Prachi Sahani <prachirsahani97@gmail.com>
2022-04-07 00:43:07 +0530

    style - responsiveness and active tab styling

--- src/Components/sidenav/SidenavItem.jsx
+++ src/Components/sidenav/SidenavItem.jsx
@@ -1,14 +1,14 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import "./sidenav.css";
 export function SidenavItem({ item }) {
   return (
-    <Link to={item.route} className="link">
+    <NavLink end={true} to={item.route} className={({isActive}) => isActive ? "link txt-primary" : "link"}>
       <li className="list-item txt-center">
         <i className="material-icons-outlined list-icon my-2">
           {item.iconName}
         </i>
         <p className="mb-2 txt-sm">{item.title}</p>
       </li>
-    </Link>
+    </NavLink>
   );
 }
